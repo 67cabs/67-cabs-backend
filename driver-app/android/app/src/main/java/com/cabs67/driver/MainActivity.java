5dbc8ba6c2caf750b9fd8df95d85d6504393b06d
Progammer1512 <urmilakbhanwar@gmail.com>
2026-08-21 15:18:47 +0530

    Fix background service, driver web push disable, and gradle optimize config

--- driver-app/android/app/src/main/java/com/cabs67/driver/MainActivity.java
+++ driver-app/android/app/src/main/java/com/cabs67/driver/MainActivity.java
@@ -1,5 +1,21 @@
 package com.cabs67.driver;
 
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Native Android Background Location Service Start
+        Intent serviceIntent = new Intent(this, LocationService.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(serviceIntent);
+        } else {
+            startService(serviceIntent);
+        }
+    }
+}
